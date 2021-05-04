cc7583e1e09345512ac7b5f31e9ed23934482ae3
Donatas Vaiciukevičius <Vaiciu.Donatas@gmail.com>
2021-05-05 01:04:27 +0300

    final comment remove

--- reservation_app/lib/splash.dart
+++ reservation_app/lib/splash.dart
@@ -13,17 +13,14 @@ class _SplashPageState extends State<SplashPage> {
   void initState() {
     SharedPreferences.getInstance().then((preferences) {
       if (!preferences.containsKey('token')) {
-        debugPrint('notoken');
         Navigator.pushReplacementNamed(context, 'login');
       } else {
         var parsedToken = parseJwt(preferences.getString('token'));
         var expirationTimestamp = parsedToken['exp'];
         var expirationTime = DateTime.fromMillisecondsSinceEpoch(expirationTimestamp * 1000);
         if (expirationTime.isAfter(DateTime.now().add(new Duration(days: -1)))) {
-          debugPrint('tokenok');
           Navigator.pushReplacementNamed(context, 'home');
         } else {
-          debugPrint('tokenShit');
           preferences.remove('user').then((_) => preferences
               .remove('token')
               .then((value) => Navigator.pushReplacementNamed(context, 'login')));
